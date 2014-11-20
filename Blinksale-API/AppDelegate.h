d0741961616f4fee877f7d44e8bdcc7ba6bce190
Mandea Daniel <daniel.mandea@yahoo.com>
2014-11-20 22:09:03 +0200

    Created Blinksale - API Demo project

---
+++ Blinksale-API/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blinksale-API
+//
+//  Created by Mandea Daniel on 20/11/14.
+//  Copyright (c) 2014 Mandea Daniel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
