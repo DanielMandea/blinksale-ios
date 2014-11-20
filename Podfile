9086ba1f94aa5ab7cf8cac8e8fa411a3d8b1523f
Mandea Daniel <daniel.mandea@yahoo.com>
2014-11-20 22:31:16 +0200

    Added Cocoapods + AFNetworking

---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, '7.0'
+
+target 'Blinksale-API' do
+
+pod 'AFNetworking', '~> 2.0'
+
+end
+
+target 'Blinksale-APITests' do
+
+end
+
---
+++ Pods/Target Support Files/Pods-Blinksale-API-AFNetworking/Pods-Blinksale-API-AFNetworking-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Blinksale_API_AFNetworking : NSObject
+@end
+@implementation PodsDummy_Pods_Blinksale_API_AFNetworking
+@end
